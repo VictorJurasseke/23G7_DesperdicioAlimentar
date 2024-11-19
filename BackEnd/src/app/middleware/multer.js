7708d9215fa62e17f53add77ccb420fdc04b15d4
VictorJurasseke <144230296+VictorJurasseke@users.noreply.github.com>
2024-11-18 22:43:53 -0300

    Update multer.js

--- BackEnd/src/app/middleware/multer.js
+++ BackEnd/src/app/middleware/multer.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) =>{
-        callback(null, path.resolve("public/Pets"))
+        callback(null, path.resolve("public/PetsClientes"))
     },
     filename: (req, file, callback) =>{
         const time = new Date().getTime();
