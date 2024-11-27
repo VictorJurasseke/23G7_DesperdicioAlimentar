57a5864e666797f4aead68329a15512d67521541
VictorJurasseke <144230296+VictorJurasseke@users.noreply.github.com>
2024-11-27 05:00:06 -0300

    Update Menu_Jogador.jsx

--- FrontEnd/src/components/TelaPerfil/Menu_Jogador.jsx
+++ FrontEnd/src/components/TelaPerfil/Menu_Jogador.jsx
@@ -21,7 +21,7 @@ const Menu_Jogador = ({ Dados_usuario, token, navigate }) => {
 
             <div className="text-center p-3 position-relative col-12 d-flex flex-column" style={{ zIndex: 2 }}>
                 <div className='col-12 d-flex flex-row justify-content-around' style={EstiloDesempenho}>
-                <div className='col-4 d-flex' style={EstiloDesempenho}>
+                <div className='d-flex' style={EstiloDesempenho}>
                     <h1>{jo_nome}</h1>
                     {/* <Desempenho Posicao={jo_rank}></Desempenho> */}
                 </div>
