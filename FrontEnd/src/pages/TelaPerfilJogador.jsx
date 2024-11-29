6aa2aa98e451c3114dfbfc920c45813c53cdfac6
VictorJurasseke <144230296+VictorJurasseke@users.noreply.github.com>
2024-11-29 02:02:43 -0300

    Update TelaPerfilJogador.jsx

--- FrontEnd/src/pages/TelaPerfilJogador.jsx
+++ FrontEnd/src/pages/TelaPerfilJogador.jsx
@@ -1,14 +1,30 @@
+// React Components
 import React, { useEffect } from 'react';
+//-----
+// Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useNavigate } from 'react-router-dom';
+import '../components/TelaPerfilJogador/CardPerfilJogador.css'
+// ----
+// Componentes 
 import Header from '../components/TelaHome/Header';
+// ----
+// Funções
 import { usePerfilDados } from '../components/TelaPerfil/FunctionTelaPerfil';
+import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import HeaderCardJogador from '../components/TelaPerfilJogador/HeaderCardJogador';
+
+//-----
+// Imagem de wireframe
 import ImagemPerfil from '../../public/img/Perfilmagem/GarotoCorteAzul.png'
+import PetPrincipal from '../../public/img/CoelhoNeymar.gif'
+
+
 // Ícones
 import { FaBookOpen } from "react-icons/fa";
+import { BiSolidTrophy } from "react-icons/bi";
 import { RiCopperCoinFill } from "react-icons/ri";
-import HeaderCardJogador from '../components/TelaPerfilJogador/HeaderCardJogador';
-import '../components/TelaPerfilJogador/CardPerfilJogador.css'
+import { FaGem } from "react-icons/fa";
 
 const PerfilJogador = () => {
     const token = localStorage.getItem("token");
@@ -21,6 +37,8 @@ const PerfilJogador = () => {
         verificarUsuario();
     }, []);
 
+    const leaves = Array.from({ length: 10 });
+
     return (
         <>
             {Dados_usuario ? (
@@ -31,50 +49,57 @@ const PerfilJogador = () => {
                     <div className="col-12 p-3 d-flex vh-100 justify-content-center align-items-center bg-light">
 
                         {/* Card principal */}
-                        <div className="col-12 col-md-6 col-lg-8 rounded shadow " style={{ backgroundColor: "#F3E8D1", marginTop: '100px' }}>
+                        <div className="col-12 col-md-10 col-lg-10 rounded shadow " style={{ backgroundColor: "#F3E8D1", marginTop: '100px' }}>
 
                             {/* Header que guarda o jogo atual e os coletados */}
                             <HeaderCardJogador jo_nome={"Temporada Primavera"} QuantidadeMascote={"26/114"} />
+
+
                             {/* Card Informações do usuario */}
-                            <div className=' d-flex flex-row flex-wrap' style={{ height: '200px' }}>
-                                <div className=' col-8 col-md-3  d-flex justify-content-center align-items-center '>
+                            <div className=' d-flex flex-row flex-wrap' style={{ minHeight: '200px' }}>
+
+
+                                {/* Div de imagem do usuário */}
+                                <div className=' col-12 col-md-4 col-lg-2   d-flex justify-content-center align-items-center '>
                                     <img
                                         src={ImagemPerfil}
                                         className="rounded-circle"
                                         alt="User"
-                                        style={{ objectFit: 'cover', height: "200px" }} // Circular e ajuste de imagem
+                                        style={{ objectFit: 'cover', maxHeight: "200px" }} // Circular e ajuste de imagem
                                     />
                                 </div>
-                                <div style={{ color: "#243447" }} className='col-4  col-md-3 py-2 align-items-start  d-flex flex-column'>
+
+                                <div style={{ color: "#243447" }} className='col-12  col-md-6 col-lg-2 p-3 text-md-center text-lg-start align-items-center  d-flex flex-column h-100'>
                                     <h1 className='jaroFont mt-2 m-0'>Victor Sales</h1>
                                     <h5 className='jaroFont m-0'>Jogador do 3 EM</h5>
-                                    <h5 className='jaroFont m-0'>Matutino</h5>
-                                    <h5 className='jaroFont m-0'>143º Lugar no ranking</h5>
-
+                                    <h5 className='jaroFont'>Pontos: <RiCopperCoinFill />300</h5>
                                 </div>
 
-                                <div className='col-6 col-md-3 p-3'>
 
-                                    {/* Barra progresso pets coletados até o momento */}
-                                    <div className="progress mt-2 m-2" style={{ height: '10px' }}>
-                                        <div
-                                            className="progress-bar"
-                                            role="progressbar"
-                                            style={{
-                                                width: `${40}%`,
-                                                backgroundColor: "#28A745",
-                                                transition: 'width 0.5s ease', // Suaviza a animação
-                                            }}
-                                            aria-valuenow={40}
-                                            aria-valuemin="0"
-                                            aria-valuemax="100"
-                                        ></div>
+                                {/* Mascote principal */}
+                                <div className='col-6 col-md-12 col-lg-4 p-3 d-flex flex-column align-items-center justify-content-center h-100'>
+                                    <img
+                                        src={PetPrincipal}
+                                        className=""
+                                        alt="PetPrincipal"
+                                        style={{ objectFit: 'contain', height: "150px" }} // Circular e ajuste de imagem
+                                    />
+                                    <p className='text-warning fs-4 jaroFont m-0'>Neylho</p>
+                                </div>
+
+                                {/* Status no jogo atual */}
+                                <div className='col-6 d-flex align-items-center flex-column  col-md-12 col-lg-4 jaroFont' style={{ color: "#243447" }}>
+
+                                    <h2 className='m-0 h-50 d-flex justify-content-end align-items-end fs-1'><BiSolidTrophy />143º</h2>
+
+
+                                    <div className='h-50 w-100 p-2 d-flex justify-content-end flex-row align-items-end'>
+
+                                        <h2 className='m-0 d-flex align-items-center'>20/117</h2>
                                     </div>
                                 </div>
-                                <div className='col-6  col-md-3 p-3'>
-                                    Desempenho
-                                </div>
                             </div>
+                            {/* CARD FIM DA TELA */}
 
                         </div>
                     </div>
@@ -87,3 +112,20 @@ const PerfilJogador = () => {
 };
 
 export default PerfilJogador;
+
+{/* Barra progresso pets coletados até o momento */ }
+{/* <div className="progress mt-2 m-2" style={{ height: '25px', border: "2px solid #243447", borderRadius: '80px', backgroundColor: "#2D4A5A" }}>
+                                        <div
+                                            className="progress-bar"
+                                            role="progressbar"
+                                            style={{
+                                                width: `${40}%`,
+                                                backgroundColor: "#D84B4B",
+                                                borderRight: "1px solid #243447",
+                                                transition: 'width 0.5s ease', // Suaviza a animação
+                                            }}
+                                            aria-valuenow={40}
+                                            aria-valuemin="0"
+                                            aria-valuemax="100"
+                                        ></div>
+                                    </div> */}
